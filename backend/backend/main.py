2b58f595aaa88e238c9a1a147a4d484fd03f1ef2
Wyatt Avilla <wyatt@linux.com>
2026-02-11 22:02:22 -0800

    backend package

---
+++ backend/backend/main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+from collections.abc import Sequence
+
+
+async def main(argv: Sequence[str] | None = None) -> None:
+    logging.basicConfig(
+        format="[%(levelname)s][%(asctime)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    logging.getLogger(__name__).info("Hello from backend!")
+    logging.getLogger(__name__).info(
+        "Received %s CLI arguments",
+        (len(argv) - 1) if argv else 0,
+    )
+
+
+def cli(argv: Sequence[str] | None = None) -> None:
+    asyncio.run(main(argv))
+
+
+if __name__ == "__main__":
+    cli()
